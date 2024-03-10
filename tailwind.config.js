926ca056f83dc3881ac154d09d394e17814c9560
Jimmy Catalano <jimmy.catalanoperso@gmail.com>
2024-03-10 10:28:20 +0100

    Ajout d'une couleur d'arrière-plan

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'background': '#45278B'
+      }
+    },
   },
   plugins: [],
 }
